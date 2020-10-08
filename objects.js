770cb23c4ff76bd7119ff8b194be42a4f7e5eea1
stellar-routine-2744 <adrianvelez1117@gmail.com>
2020-10-08 03:21:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
   var playlist = new Object({Slowdive: 'Alison'});
   
   Function updatePlaylist(object, key, value) {
-    Object.assign({My Bloody Valentine: 'sometiems'})
+    return Object.assign({}, {[My Bloody Valentine]: 'sometiems'})
   }
   
   
