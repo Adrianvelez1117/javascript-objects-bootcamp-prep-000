fe64477b8b2c546df0921be1531cac07904772a2
stellar-routine-2744 <adrianvelez1117@gmail.com>
2020-10-08 02:58:55 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
   var playlist = new Object({michaelJackson: "triller"});
   
+  var updatePlaylist = new Object(playlist, {vicente: "sigoSiendo"});
   
-  
-  Function updatePlaylist(playlist, {vicente: "sigoSiendo"} ) {
+  Function updatePlaylist(playlist, artist, song ) {
     return Object.assign ({}, updatePlaylist)
     }
     
